e64acf1f336a01cd378d7ff800e454d3fe311520
Richard Xu <richard.xu@roadbotics.com>
2018-03-14 15:26:33 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { jsonServerRestClient, Admin, Resource, Delete } from 'admin-on-rest';
+
+import { PostList, PostEdit, PostCreate } from './posts';
+import { UserList } from './users';
+import Dashboard from './Dashboard';
+import authClient from './authClient';
+import PostIcon from 'material-ui/svg-icons/action/book';
+import UserIcon from 'material-ui/svg-icons/social/group';
+
+const App = () => (
+  <Admin authClient={authClient} dashboard={Dashboard} restClient={jsonServerRestClient('http://jsonplaceholder.typicode.com')}>
+    <Resource name="posts" list={PostList} edit={PostEdit} create={PostCreate} remove={Delete} icon={PostIcon} />
+    <Resource name="users" list={UserList} remove={Delete} icon={UserIcon} />
+  </Admin>
+)
+
+export default App;
